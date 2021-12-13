7e815e6bbcb39caf104c8a318a7bfac93b567e5c
fabio.ortiz.mt@correo.usa.edu.co <nombre-usuario@correo.com>
2021-12-13 00:08:57 -0500

    m

--- src/main/java/Reto2_Web/controlador/UserController.java
+++ src/main/java/Reto2_Web/controlador/UserController.java
@@ -1,14 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Reto2_Web.controlador;
+
 import Reto2_Web.servicio.UserService;
 import Reto2_Web.modelo.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +34,12 @@ public class UserController {
     public List<User> getAll() {
         return userService.getAll();
     }
+    
+     @GetMapping("/{id}")
+    public Optional <User> getUser(@PathVariable("id") int id) {
+        return userService.getUser(id);
+    }
+    
       @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody User user) {
